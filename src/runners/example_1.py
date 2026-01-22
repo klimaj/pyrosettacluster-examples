9773bf96c438b072f260e3601d277773434e4ba1
Jason C. Klima <klimaj@uw.edu>
2026-01-21 16:02:30 -0800

    Update main

--- src/runners/example_1.py
+++ src/runners/example_1.py
@@ -39,29 +39,39 @@ def get_input_packed_pose() -> PackedPose:
 
 def main(
     output_path: str,
-    num_tasks: int = 5,
+    num_tasks: int = 6,
     num_protocols: int = 3,
 ) -> None:
     """Run the PyRosettaCluster example #1 simulation."""
+    # Initialize PyRosetta
     pyrosetta.init("-run:constant_seed 1")
 
+    # Set the number of workers dynamically
+    n_workers = os.cpu_count()
+    print(f"Spinning up {n_workers} dask workers.")
+
+    # Run the simulation
     with LocalCluster(
-        n_workers=os.cpu_count(),
+        n_workers=n_workers,
         threads_per_worker=1,
         memory_limit="4GB",
         scheduler_port=8786,
         dashboard_address=":8787",
+        resources={"CPU": 1},
     ) as cluster, Client(cluster) as client:
         PyRosettaCluster(
             tasks=create_tasks(num_tasks),
             input_packed_pose=get_input_packed_pose(),
             client=client,
             output_path=output_path,
+            project_name="pyrosettacluster-examples",
+            simulation_name="example-1",
+            simulation_records_in_scorefile=True,
         ).distribute(
             protocols=[test_protocol] * num_protocols,
             clients_indices=None,
-            priorities=None,
-            resources=None,
+            priorities=tuple(range(num_protocols)),
+            resources=[{"CPU": 1}] * num_protocols,
         )
 
 
