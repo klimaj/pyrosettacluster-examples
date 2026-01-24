f752760f02f769c0b5317ebb880210f8bafb5fbb
Jason C. Klima <klimaj@uw.edu>
2026-01-24 14:47:33 -0800

    Initial commit

---
+++ src/plotting/analyze_example_1.py
@@ -0,0 +1,52 @@
+__author__ = "Jason C. Klima"
+
+
+import argparse
+
+from pathlib import Path
+
+from src.utils import get_dataframe
+
+
+def main(original_scorefile: Path, scorefxn: str) -> None:
+    """Print info about the lowest and highest energy decoys."""
+    df = get_dataframe(original_scorefile)
+    x = "rmsd_all_heavy"
+    if scorefxn == "beta_nov16":
+        y = "total_score"
+    elif scorefxn == "ref2015":
+        y = "total_score_ref2015"
+    else:
+        raise ValueError(f"Scorefunction is not supported: '{scorefxn}'")
+    c = "seed"
+    idx_min = df[y].idxmin()
+    x_min = df.loc[idx_min, x]
+    y_min = df.loc[idx_min, y]
+    c_min = df.loc[idx_min, c]
+    print(f"Lowest energy decoy ({x_min}, {y_min}, {int(c_min )}):", df.loc[idx_min, "output_file"])
+    idx_max = df[y].idxmax()
+    x_max = df.loc[idx_max, x]
+    y_max = df.loc[idx_max, y]
+    c_max = df.loc[idx_max, c]
+    print(f"Highest energy decoy ({x_max}, {y_max}, {int(c_max)}):", df.loc[idx_max, "output_file"])
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Analyze PyRosettaCluster usage example #1 results.",
+    )
+    parser.add_argument(
+        "--original_scorefile",
+        type=Path,
+        required=True,
+        help="The original PyRosettaCluster simulation output scorefile (i.e., a 'scores.json' file).",
+    )
+    parser.add_argument(
+        "--scorefxn",
+        type=str,
+        required=False,
+        default="beta_nov16",
+        help="The scorefunction value to plot.",
+    )
+    args = parser.parse_args()
+    main(args.original_scorefile, args.scorefxn)
