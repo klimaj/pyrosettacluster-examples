a59ccb520ed4d69a07763dfd0e32f6b30d28a985
Jason C. Klima <klimaj@uw.edu>
2026-01-30 22:55:15 -0800

    Initial commit

---
+++ src/plotting/analyze_example_2.py
@@ -0,0 +1,36 @@
+__author__ = "Jason C. Klima"
+
+
+import argparse
+
+from pathlib import Path
+
+from src.utils import get_dataframe_from_pickle
+
+
+def main(original_scorefile: Path) -> None:
+    """Print info about the lowest RMSD decoy."""
+    v = (
+        get_dataframe_from_pickle(original_scorefile)
+        .sort_values("bb_rmsd", ascending=True)
+        .reset_index(drop=True)
+        .iloc[0] # Top ranked design
+    )
+    bb_rmsd = v["bb_rmsd"]
+    total_score = v["total_score"]
+    output_file = v["output_file"]
+    print(f"Lowest RMSD decoy (bb_rmsd={bb_rmsd}; total_score={total_score}):", output_file)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Analyze PyRosettaCluster usage example #2 results.",
+    )
+    parser.add_argument(
+        "--original_scorefile",
+        type=Path,
+        required=True,
+        help="The original PyRosettaCluster simulation output pickled `pandas.DataFrame` scorefile (i.e., a 'scores.bz2' file).",
+    )
+    args = parser.parse_args()
+    main(args.original_scorefile)
