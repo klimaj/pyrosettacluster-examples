b604d6af531c43ffe1bccbfee4518efa0cdcb397
Jason C. Klima <klimaj@uw.edu>
2026-02-01 15:52:32 -0800

    Initial commit

---
+++ src/plotting/get_bb_rmsd_nosuper_example_2.py
@@ -0,0 +1,32 @@
+__author__ = "Jason C. Klima"
+
+
+import argparse
+
+from pathlib import Path
+
+from src.utils import get_bb_rmsd_nosuper, get_dataframe_from_pickle
+
+
+def main(original_decoy: Path, reproduce_decoy: Path) -> None:
+    """Print info about the lowest RMSD decoy."""
+    bb_rmsd = get_bb_rmsd_nosuper(original_decoy, reproduce_decoy)
+    print(bb_rmsd)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Analyze PyRosettaCluster usage example #2 results.",
+    )
+    parser.add_argument(
+        "--original_decoy",
+        type=Path,
+        required=True,
+    )
+    parser.add_argument(
+        "--reproduce_decoy",
+        type=Path,
+        required=True,
+    )
+    args = parser.parse_args()
+    main(args.original_decoy, args.reproduce_decoy)
